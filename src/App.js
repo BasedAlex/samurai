17772937a5bb156369c1b51e504c15cf944a9799
Based <alexbroseph@mail.ru>
2022-03-03 20:04:48 +0400

    12. Уроки React JS (Разбивка на компоненты)

--- src/App.js
+++ src/App.js
@@ -1,43 +1,15 @@
 import React from 'react'
 import './App.css'
+import Header from './components/Header'
+import Navbar from './components/Navbar'
+import Profile from './components/Profile'
 
 const App = () => {
 	return (
 		<div className='app-wrapper'>
-			<header className='header'>
-				<img src='#' />
-			</header>
-			<nav className='nav'>
-				<div>
-					<a href='#'>Profile</a>
-				</div>
-				<div>
-					<a href='#'>Messages</a>
-				</div>
-				<div>
-					<a href='#'>News</a>
-				</div>
-				<div>
-					<a href='#'>Music</a>
-				</div>
-				<div>
-					<a href='#'>Settings</a>
-				</div>
-			</nav>
-			<div className='content'>
-				<div>
-					<img src='#' />
-				</div>
-				<div>ava + description</div>
-				<div>
-					My posts
-					<div>New posts</div>
-					<div>
-						<div>Post 1</div>
-						<div>Post 2</div>
-					</div>
-				</div>
-			</div>
+			<Header />
+			<Navbar />
+			<Profile />
 		</div>
 	)
 }
