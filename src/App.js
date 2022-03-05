17b1a2047aa270e12143cf6393291c69765022b8
Based <alexbroseph@mail.ru>
2022-03-05 15:11:03 +0400

    18. Уроки React JS (страница Dialogs)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import './App.css'
+import Dialogs from './components/Dialogs/DIalogs'
 import Header from './components/Header/Header'
 import Navbar from './components/Navbar/Navbar'
-import Profile from './components/Profile/Profile'
 
-const App = () => {
+// import Profile from './components/Profile/Profile'
+
+const App = props => {
 	return (
 		<div className='app-wrapper'>
 			<Header />
 			<Navbar />
-			<Profile />
+			<div class='app-wrapper-content'>
+				{/* <Profile /> */}
+				<Dialogs />
+			</div>
 		</div>
 	)
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import MyPosts from './MyPosts/MyPosts'
 
 const Profile = () => {
 	return (
-		<div className={s.content}>
+		<div>
 			<div>
 				<img
 					src='https://i.pinimg.com/originals/0b/7c/fd/0b7cfdaa80abeb33b06e1d59e12c1245.jpg'
