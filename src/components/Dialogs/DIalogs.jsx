656e1d1556ca86d8fd47a2b6e550a7b947e12e14
Based <alexbroseph@mail.ru>
2022-03-06 03:10:23 +0400

    21. Уроки React JS (Верстаем страницу Диалогов)

--- src/components/Dialogs/DIalogs.jsx
+++ src/components/Dialogs/DIalogs.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 import s from './Dialogs.module.css'
 
-const Dialogs = () => {
-	return <div>Dialogs</div>
+const Dialogs = props => {
+	return (
+		<div className={s.dialogs}>
+			<div className={s.dialog__items}>
+				<div className={s.dialog + ' ' + s.active}>Alex</div>
+				<div className={s.dialog}>Dima</div>
+				<div className={s.dialog}>Valera</div>
+				<div className={s.dialog}>Jora</div>
+				<div className={s.dialog}>Kirill</div>
+			</div>
+			<div className={s.messages}>
+				<div className={s.message}>Hi</div>
+				<div className={s.message}>How is your nothing?</div>
+				<div className={s.message}>YO</div>
+			</div>
+		</div>
+	)
 }
 
 export default Dialogs
