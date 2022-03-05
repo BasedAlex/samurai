34bc18115036ed8de0a3b7a53b7fb24abe622fd8
Based <alexbroseph@mail.ru>
2022-03-05 22:27:06 +0400

    20. Уроки React JS (NavLink)

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,36 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import s from './Navbar.module.css'
+
+const links = navData => (navData.isActive ? s.active : s.item)
 
 const Navbar = () => {
 	return (
 		<nav className={s.nav}>
-			<div className={s.item}>
-				<a href='/profile'>Profile</a>
+			<div>
+				<NavLink to='/profile' className={links}>
+					Profile
+				</NavLink>
 			</div>
-			<div className={`${s.item} ${s.active}`}>
-				<a href='/dialogs'>Messages</a>
-			</div>
-			<div className={s.item}>
-				<a>News</a>
-			</div>
-			<div className={s.item}>
-				<a>Music</a>
+			<div>
+				<NavLink to='/dialogs' className={links}>
+					Messages
+				</NavLink>
 			</div>
 			<div className={s.item}>
-				<a>Settings</a>
+				<NavLink to='/news' className={links}>
+					News
+				</NavLink>
+			</div>
+			<div className={s.item}>
+				<NavLink to='/music' className={links}>
+					Music
+				</NavLink>
+			</div>
+			<div className={s.item}>
+				<NavLink to='/settings' className={links}>
+					Settings
+				</NavLink>
 			</div>
 		</nav>
 	)
