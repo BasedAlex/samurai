68efad3aa63956347a26ac0b1df620a17932d377
Based <alexbroseph@mail.ru>
2022-03-06 19:29:43 +0400

    24. Уроки React JS (UI - BLL, выносим данные в сторону)

--- src/components/Dialogs/DIalogs.jsx
+++ src/components/Dialogs/DIalogs.jsx
@@ -17,21 +17,37 @@ const Message = props => {
 }
 
 const Dialogs = props => {
+	let dialogsData = [
+		{ id: 1, name: 'Alex' },
+		{ id: 2, name: 'Dima' },
+		{ id: 3, name: 'Valera' },
+		{ id: 4, name: 'Jora' },
+		{ id: 5, name: 'Kirill' },
+	]
+
+	let messagesData = [
+		{ id: 1, message: 'Hi' },
+		{ id: 2, message: 'How is your nothing?' },
+		{ id: 3, message: 'Yo' },
+		{ id: 4, message: 'Yo' },
+		{ id: 5, message: 'Yo' },
+	]
+
 	return (
 		<div className={s.dialogs}>
 			<div className={s.dialog__items}>
 				<div className={s.dialog}>
-					<DialogItem name='Alex' id='1' />
-					<DialogItem name='Dima' id='2' />
+					<DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
+					<DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
 					<DialogItem name='Valera' id='3' />
 					<DialogItem name='Jora' id='4' />
 					<DialogItem name='Kirill' id='5' />
 				</div>
 			</div>
 			<div className={s.messages}>
-				<Message message='Hi!' />
-				<Message message='How is your nothing?' />
-				<Message message='Yo!' />
+				<Message message={messagesData[0].message} />
+				<Message message={messagesData[1].message} />
+				<Message message={messagesData[2].message} />
 			</div>
 		</div>
 	)
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,14 @@ import s from './MyPosts.module.css'
 import Post from './Posts/Post'
 
 const MyPosts = () => {
+	let postData = [
+		{ id: 1, message: 'Hi, how are you?', likes: 10 },
+		{ id: 2, message: "It's my first post!", likes: 0 },
+		{ id: 3, message: 'Yo' },
+		{ id: 4, message: 'Yo' },
+		{ id: 5, message: 'Yo' },
+	]
+
 	return (
 		<div class={s.postsBlock}>
 			<h3>My posts</h3>
@@ -15,8 +23,16 @@ const MyPosts = () => {
 				</div>
 			</div>
 			<div className={s.posts}>
-				<Post message='Hi, how are you?' likes='10' className={s.post} />
-				<Post message="It's my first post!" likes='0' className={s.post} />
+				<Post
+					message={postData[0].message}
+					likes={postData[0].likes}
+					className={s.post}
+				/>
+				<Post
+					message={postData[1].message}
+					likes={postData[1].likes}
+					className={s.post}
+				/>
 			</div>
 		</div>
 	)
