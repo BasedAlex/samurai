8723e71fc74d70a08db4ba66b04ca6b65135953d
Based <alexbroseph@mail.ru>
2022-03-12 17:07:11 +0400

    42 - React JS Практика - Redux

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 import './index.css'
 // import App from './App'
 
-import store from './redux/state'
+import store from './redux/redux-store'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
@@ -14,6 +14,7 @@ import reportWebVitals from './reportWebVitals'
 // import state from './redux/state'
 
 let rerenderEntireTree = state => {
+	debugger
 	ReactDOM.render(
 		<React.StrictMode>
 			<App state={state} dispatch={store.dispatch.bind(store)} store={store} />
@@ -24,7 +25,10 @@ let rerenderEntireTree = state => {
 
 rerenderEntireTree(store.getState())
 
-store.subscribe(rerenderEntireTree)
+store.subscribe(() => {
+	let state = store.getState()
+	rerenderEntireTree(state)
+})
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,7 +1,25 @@
 const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY'
 const SEND_MESSAGE = 'SEND-MESSAGE'
 
-const dialogsReducer = (state, action) => {
+let initialState = {
+	items: [
+		{ id: 1, user: 'Alex', message: 'Hi', side: 'main' },
+		{ id: 2, user: 'Alex', message: 'How is your nothing?', side: 'main' },
+		{ id: 3, user: 'Alex', message: 'Yo', side: 'main' },
+		{ id: 4, user: 'Alex', message: 'Yo', side: 'guest' },
+		{ id: 5, user: 'Alex', message: 'Fine thank you! hbu?', side: 'guest' },
+	],
+	dialogs: [
+		{ id: 1, name: 'Alex' },
+		{ id: 2, name: 'Dima' },
+		{ id: 3, name: 'Valera' },
+		{ id: 4, name: 'Jora' },
+		{ id: 5, name: 'Kirill' },
+	],
+	newMessageBody: '',
+}
+
+const dialogsReducer = (state = initialState, action) => {
 	switch (action.type) {
 		case UPDATE_NEW_MESSAGE_BODY:
 			state.newMessageBody = action.body
