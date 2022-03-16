c5a3dde557d64bc074ee0281e7264c9d050f6706
Based <alexbroseph@mail.ru>
2022-03-16 18:48:51 +0400

    added a button

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,17 +4,19 @@ import styles from './users.module.css'
 import userPhoto from '../../assets/image/user.png'
 
 let Users = props => {
-	if (props.users.length === 0) {
-		axios
-			.get('https://social-network.samuraijs.com/api/1.0/users')
-			.then(response => {
-				props.setUsers(response.data.items)
-			})
-		// props.setUsers()
+	let getUsers = () => {
+		if (props.users.length === 0) {
+			axios
+				.get('https://social-network.samuraijs.com/api/1.0/users')
+				.then(response => {
+					props.setUsers(response.data.items)
+				})
+		}
 	}
 
 	return (
 		<div>
+			<button onClick={getUsers}>Get Users</button>
 			{props.users.map(u => (
 				<div key={u.id}>
 					<span>
