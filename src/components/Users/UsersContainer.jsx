bc07ce1a2aa1c27bbf86dde7f69f55a8d474f776
Based <alexbroseph@mail.ru>
2022-03-17 15:47:39 +0400

    55: pagination for users page

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { followAC, setUsersAC, unfollowAC } from '../../redux/users-reducer'
+import {
+	followAC,
+	setCurrentPageAC,
+	setTotalUsersCountAC,
+	setUsersAC,
+	unfollowAC,
+} from '../../redux/users-reducer'
 import Users from './Users'
 
 let mapStateToProps = state => {
 	return {
 		users: state.userPage.users,
+		pageSize: state.userPage.pageSize,
+		totalUsersCount: state.userPage.totalUsersCount,
+		currentPage: state.userPage.currentPage,
 	}
 }
 
@@ -19,6 +28,12 @@ let mapDispatchToProps = dispatch => {
 		},
 		setUsers: users => {
 			dispatch(setUsersAC(users))
+		},
+		setCurrentPage: pageNumber => {
+			dispatch(setCurrentPageAC(pageNumber))
+		},
+		setTotalUsersCount: totalCount => {
+			dispatch(setTotalUsersCountAC(totalCount))
 		},
 	}
 }
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,9 +1,14 @@
 const FOLLOW = 'FOLLOW'
 const UNFOLLOW = 'UNFOLLOW'
 const SET_USERS = 'SET_USERS'
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE'
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT'
 
 let initialState = {
 	users: [],
+	pageSize: 5,
+	totalUsersCount: 0,
+	currentPage: 2,
 }
 
 export const usersReducer = (state = initialState, action) => {
@@ -33,7 +38,17 @@ export const usersReducer = (state = initialState, action) => {
 		case SET_USERS:
 			return {
 				...state,
-				users: [...state.users, ...action.users],
+				users: [action.users],
+			}
+		case SET_CURRENT_PAGE:
+			return {
+				...state,
+				currentPage: action.currentPage,
+			}
+		case SET_TOTAL_USERS_COUNT:
+			return {
+				...state,
+				totalUsersCount: action.count,
 			}
 		default:
 			return state
@@ -46,5 +61,14 @@ export const unfollowAC = userID => ({
 	userID,
 })
 export const setUsersAC = users => ({ type: SET_USERS, users })
+
+export const setCurrentPageAC = currentPage => ({
+	type: SET_CURRENT_PAGE,
+	currentPage,
+})
+export const setTotalUsersCountAC = totalUsersCount => ({
+	type: SET_TOTAL_USERS_COUNT,
+	count: totalUsersCount,
+})
 
 export default usersReducer
